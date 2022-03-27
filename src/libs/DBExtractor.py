bbdc23ce2868ed8a0ab69a703209812d57390024
Rodrigo Vega <rvega@colway-08.com>
2022-03-27 23:53:50 +0200

    Item Source added

--- src/libs/DBExtractor.py
+++ src/libs/DBExtractor.py
@@ -36,7 +36,7 @@ class DBExtractor():
             cursor.execute("select * from Item where ItemId between 1 and 20")
             data = cursor.fetchall()
             
-            header = ['ItemId', 'ItemDocumentNbr','CustomerName','CreateDate', 'UpdateDate']
+            header = ['ItemId', 'ItemDocumentNbr','CustomerName','CreateDate', 'UpdateDate']#, 'ItemSource']
             
             with open('list.csv', 'w', encoding='UTF8') as f:
                 writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -54,11 +54,14 @@ class DBExtractor():
                     customer = row[3]
                     
                     if row == rows[-1] or row[0] != rows[i+1][0]: ##check if it is the last row
+                        row = list(row)
                         if customer.startswith("99"):
-                            print('LOCAL')
+                            row.append('Local')
+                        
                         else:
-                            print('EXTERNAL')
-                            
+                            row.append('External')
+                        row = tuple(row)    
+                        print(row)
                         row = row[:4] + row[5:]
                         row = row[:2] + row[3:]
                         
@@ -70,6 +73,8 @@ class DBExtractor():
             with open('list.csv', 'rb') as f_in: ##GZIP compress
                 with gzip.open('list.csv.gz', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
+            f_in.close()
+            f_out.close() 
             #
             # End of exercise
         except:
